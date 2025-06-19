4d34afd4255670c2f6393c99ea8c79c5874bf6ff
Your Name <maratog@gmail.com>
2025-06-19 12:13:12 -0600

    Correcta funcionalidad del js y modificación de links.

--- animacion.js
+++ animacion.js
@@ -1,7 +1,21 @@
 let control = document.querySelector('#imagen_controlcito');
 let currentRotation = 0;
+let correo = document.querySelector('#correito');
 
-control.addEventListener('çlick', () => {
-    currentRotation += 90;
-    control.style.transform = `rotate(${currentRotation}deg)`;
-}); 
+//Función de rotación del controlsito
+control.addEventListener('click', () => {
+    currentRotation += 360;
+    control.style.transform = `rotate(${currentRotation}deg)`; 
+});
+
+// Función de copiado en portapapeles del correo
+correo.addEventListener('click', () => {
+        const texto = correo.textContent;
+    navigator.clipboard.writeText(texto)
+        .then(() => {
+        alert('Correo copiado al portapapeles');
+        })
+        .catch(err => {
+        console.error('Error al copiar: ', err);
+        });
+});
